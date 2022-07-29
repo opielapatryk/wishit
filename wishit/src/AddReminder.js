972a15d8db2d00f2f22dedf231a7bf027b0c3553
opielapatryk <xpatrykopiela@gmail.com>
2022-07-29 16:03:43 +0200

    e

--- wishit/src/AddReminder.js
+++ wishit/src/AddReminder.js
@@ -13,7 +13,7 @@ function AddReminder() {
         </header>
         <section>
           <label for="birthday-date">Select birthday date: </label>
-          <input type="date" id="birthday-date"/><br/>
+          <input type="date" id="birthday-date" placeholder="12.12.2012"/><br/>
           <label for="birthday-name">Select birthday name: </label>
           <input type="text" id="birthday-name" placeholder="Insert name of your friend"/><br/>
           <input type='submit' id='submit' value='Add!'/>

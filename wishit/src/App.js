a25bc69ae874abc5305c1c1524249d79db9f5374
opielapatryk <xpatrykopiela@gmail.com>
2022-07-29 12:33:02 +0200

    header

--- wishit/src/App.js
+++ wishit/src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="main">
-
+        <header>
+          <h1>
+          WishIT! :)
+          </h1>
+          <h1>
+          Here you can add new birthday reminders!
+          </h1>
+        </header>
     </div>
   );
 }

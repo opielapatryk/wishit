9fbbbdd5d61d1ddc26acc150070c21742cd3da84
opielapatryk <xpatrykopiela@gmail.com>
2022-07-29 12:56:36 +0200

    header

--- wishit/src/App.js
+++ wishit/src/App.js
@@ -2,16 +2,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="main">
+    <main>
         <header>
-          <h1>
+          <h1 clas>
           WishIT! :)
           </h1>
-          <h1>
+          <h3>
           Here you can add new birthday reminders!
-          </h1>
+          </h3>
         </header>
-    </div>
+        <section>
+          <label for="birthday-date">Select birthday date: </label>
+          <input type="date" id="birthday-date"/><br/>
+          <label for="birthday-name">Select birthday name: </label>
+          <input type="text" id="birthday-name" placeholder="Insert name of your friend"/><br/>
+          <input type='submit' id='submit' value='Add!'/>
+        </section>        
+    </main>
   );
 }
 

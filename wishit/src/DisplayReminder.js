9b808ebec9ddcc1e8d5d9c49325920ef0c67a540
opielapatryk <xpatrykopiela@gmail.com>
2022-08-08 21:15:20 +0200

    e

--- wishit/src/DisplayReminder.js
+++ wishit/src/DisplayReminder.js
@@ -1,15 +1,26 @@
 import './DisplayReminder.css';
-
+import birthdays from './listOfBirthdays';
 let today = new Date();
 let dd = String(today.getDate()).padStart(2, '0');
 let mm = String(today.getMonth() + 1).padStart(2, '0'); 
 let yyyy = today.getFullYear();
 today = mm + '.' + dd + '.' + yyyy;
-let birthdayToday = ['someone', 'somebody']
+let todaywithoutyear =  mm + '.' + dd
+
+let birthdayToday = function(){
+  let listofpersons = []
+  birthdays.forEach((e)=>{
+    if(e.date === todaywithoutyear){
+      listofpersons.push(e.name)
+    }
+  })
+  return listofpersons.join("\n")
+}
+
 function DisplayReminder() {
   return (
     <main>
-        <h1 className='display-reminder-header'>Today is {today} and birthday have...<br/> {birthdayToday.join(', ')}</h1>
+        <h1 className='display-reminder-header'>Today is {today} and birthday have...<br/></h1> <h2 className='listofbirth'>{birthdayToday()}</h2>
     </main>
   );
 }
---
+++ wishit/src/listOfBirthdays.js
@@ -0,0 +1,8 @@
+let birthdays = 
+        [{
+            date: "13.08",
+            name: "Szymon Bolek"
+    }
+]
+
+export default birthdays
